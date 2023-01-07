cc74e2e5203ac010181634abb869592e92689e3d
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2023-01-07 17:50:10 -0400

    Font-face, buttons, and font-sizes responsive set

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import htmlPurge from "vite-plugin-html-purgecss";
+import htmlPurge from 'vite-plugin-html-purgecss';
 
 export default {
   plugins: [htmlPurge()],
+  base: '/single-page-portfolio/',
 };
 
 // npm i vite-plugin-html-purgecss -D
